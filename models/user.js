9bdefc503e9487aad690c3957af3c5f90e8cbc42
이정현 <ijeonghyeon@ijeonghyeon-ui-MacBookPro.local>
2020-08-19 12:50:37 +0900

    register+delete

--- models/user.js
+++ models/user.js
@@ -2,19 +2,22 @@ const query = require('../helper/db/query');
 
 module.exports = {
     register: async (data) => {
-        const selectQ = "";
+       const selectQ = `INSERT INTO twitterdb.USER(name, password,email,phonenumber, gender, registerdate,profile) VALUES('${data.name}','${data.password}','${data.email}','${data.phonenumber}','${data.gender}',NOW(),'null')`;
         return await query.selectSync(selectQ);
     },
     login: async(data) => {
+        //`select * from twitterdb.USER where email = '${userInfo.email}'and password = '${userInfo.password}'`
         const selectQ = "";
         return await query.selectSync(selectQ);
     },
     update: async(data) => {
+        //`update USER set name ='${userInfo.name}', phonenumber='${userInfo.phonenumber}',gender='${userInfo.gender}', profile='${userInfo.profile}' where id='${userInfo.id}'`;
         const updateQ = "";
         return await query.querySync(updateQ);
     },
     delete: async (data) => {
-        const deleteQ = "";
+        //`DELETE FROM USER WHERE email = '${userInfo.email}' and password = '${userInfo.password}'`
+        const deleteQ = `DELETE FROM USER WHERE email = '${data.email}' and password = '${data.password}'`;
         return await query.querySync(deleteQ);
     }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var hash = require('../models/hash').hashValue;
+var query = require('../models/user');
 
 /* GET users listing. */
 router.get('/', (req, res) => {
@@ -8,32 +10,71 @@ router.get('/', (req, res) => {
 
 /* Registration page */
 router.get('/register', (req, res) => {
-  res.send(req.method + " " + req.originalUrl);
+  res.render('register');
+  //res.send(req.method + " " + req.originalUrl);
 });
 
 /* Register user api */
 router.post('/register', (req, res) => {
-  res.send(req.method + " " + req.originalUrl);
+  const hashpassword = hash(req.body.email,req.body.password);
+
+  const userInfo = {
+    name : req.body.name,
+    email : req.body.email,
+    password : hashpassword,
+    phonenumber : req.body.phonenumber,
+    gender : req.body.gender
+  }
+    query.register(userInfo)
+        .then(result => {
+            res.send('<script>alert("가입되었습니다");location.href="./login";</script>');
+        }).catch(err => {
+          res.send('<script>alert("이미 존재하는 이메일 입니다.");location.href="./register";</script>');
+    })
 });
 
-/* User profile page */
-router.get('/profile', (req, res) => {
-  res.send(req.method + " " + req.originalUrl);
+/* Delete page */
+router.get('/delete', function (req,res,next) {
+  var inform = req.session.user;
+  res.render('delete',inform);
 });
 
-/* Detele user api */
-router.delete('/', (req, res) => {
-  res.send(req.method + " " + req.originalUrl);
+/* Delete user api */
+router.post('/delete', function (req,res,next) {
+
+  const hashpassword  = hash(req.session.user.email,req.body.password);
+
+  const userInfo = {
+    email : req.session.user.email,
+    password : hashpassword,
+  }
+  query.delete((userInfo))
+      .then(result => {
+        res.send('<script>alert("삭제 되었습니다"); location.href="./home";</script>');
+      }).catch(err => {
+        res.send('<script>alert("비밀번호가 일치하지 않습니다.");location.href="./delete";</script>');
+      });
 });
 
 /* Login */
 router.get('/login', (req, res) => {
-  res.send(req.method + " " + req.originalUrl);
+  //res.send(req.method + " " + req.originalUrl);
 });
 
 /* Login api */
 router.post('/login', (req, res) => {
-  res.send(req.method + " " + req.originalUrl);
+  //res.send(req.method + " " + req.originalUrl);
 });
+
+/* profile */
+router.get('/profile', (req, res) => {
+  //res.send(req.method + " " + req.originalUrl);
+});
+
+/* profile api */
+router.post('/profile', (req, res) => {
+  //res.send(req.method + " " + req.originalUrl);
+});
+
 
 module.exports = router;
